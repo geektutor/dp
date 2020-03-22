d6be6f111b2945fa742dbbc9c1b6914a07a0a094
geektutor <sodiq.akinjobi@gmail.com>
2020-03-22 17:38:27 +0100

    Started working on canvas

--- src/js/name.js
+++ src/js/name.js
@@ -1,83 +1,21 @@
-$(function() {
-    // Get name from URL
-    function getUrlVars() {
-        var vars = {};
-        var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-            vars[key] = value;
-    });
-        return vars;
-    }
-    var name = getUrlVars()["x"];
-    var surname = getUrlVars()['text'];
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
+    vars[key] = value;
+  });
+  return vars;
+}
+  var name = getUrlVars()["x"];
+  var surname = getUrlVars()['text'];
 
-    function b64toBlob(b64Data, contentType, sliceSize) {
-      contentType = contentType || "";
-      sliceSize = sliceSize || 512;
-  
-      var byteCharacters = atob(b64Data);
-      var byteArrays = [];
-  
-      for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-        var slice = byteCharacters.slice(offset, offset + sliceSize);
-  
-        var byteNumbers = new Array(slice.length);
-        for (var i = 0; i < slice.length; i++) {
-          byteNumbers[i] = slice.charCodeAt(i);
-        }
-  
-        var byteArray = new Uint8Array(byteNumbers);
-        byteArrays.push(byteArray);
-      }
-  
-      var blob = new Blob(byteArrays, { type: contentType });
-      return blob;
-    }
-
-    function createDP(name, imageUrl, cb) {
-      var canvas = document.createElement("canvas"),
-        ctx = canvas.getContext("2d"),
-        imageCount = 2,
-        viewW = 800,
-        viewH = 800;
-  
-      var userImg = loadImage(imageUrl);
-      var frameImg = loadImage("src/img/frame.jpeg");
-  
-      function loadImage(src) {
-        var img = new Image();
-        img.onload = transformImage;
-        img.src = src;
-        return img;
-      }
-  
-      function transformImage() {
-        if (--imageCount !== 0) return;
-  
-        canvas.width = frameImg.width;
-        canvas.height = frameImg.height;
-  
-        ctx.drawImage(frameImg, 0, 0);
-  
-        ctx.drawImage(userImg, 0, 0, viewW, viewH);
-  
-        cb(canvas.toDataURL());
-      }
-    }
-  
-    function navigateTo(view, temp = "") {
-      switch (view) {
-        case "yourdp":
-          main.html(temp);
-          main.css({ background: "none" });
-          break;
-        default:
-          main.style.background = "rgb(108, 86, 123)";
-          main.innerHTML = mainContent;
-      }
-    }
-  
-    console.log("DOM fully loaded and parsed");
-    console.log(name)
-    console.log(surname)
-});
-  
+window.onload = function(){
+  var canvas = document.getElementById("myCanvas");
+  var context = canvas.getContext("2d");
+  var imageObj = new Image();
+  imageObj.onload = function(){
+      context.drawImage(imageObj, 10, 10);
+      context.font = "40pt Calibri";
+      context.fillText(name, 20, 20);
+  };
+  imageObj.src = "src/img/frame.jpeg"; 
+};
